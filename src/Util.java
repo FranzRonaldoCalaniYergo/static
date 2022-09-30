3f3f1c267acb1c9f8d1216fbb55e343e8688aca1
FranzRonaldoCalaniYergo <gohankillua07@gmail.com>
2022-09-30 09:58:39 -0400

    static

---
+++ src/Util.java
@@ -0,0 +1,8 @@
+public class Util {
+    public static final double DOLARES = 9.91;
+    public static final double EUROS = 6.73;
+    public static final double ARGENTINO = 21.33;
+    public static double bolivianosPesos(double bolivianos) {
+        return bolivianos * ARGENTINO;
+    }
+}
